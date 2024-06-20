d7d95436ce0d1f5ed4e34bdab713c935f558a983
toastoffee <931391442@qq.com>
2024-06-20 19:26:50 +0800

    add thread pool

---
+++ thread_pool.hpp
@@ -0,0 +1,18 @@
+/**
+  ******************************************************************************
+  * @file           : thread_pool.hpp
+  * @author         : toastoffee
+  * @brief          : None
+  * @attention      : None
+  * @date           : 2024/6/20
+  ******************************************************************************
+  */
+
+
+
+#ifndef THREAD_POOL_TOOLS_THREAD_POOL_HPP
+#define THREAD_POOL_TOOLS_THREAD_POOL_HPP
+
+
+
+#endif //THREAD_POOL_TOOLS_THREAD_POOL_HPP
