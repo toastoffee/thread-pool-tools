bddb167f1608114bcc375bc0f2b780feb0447dcc
toastoffee <931391442@qq.com>
2024-06-21 23:49:51 +0800

    update desc

--- README.md
+++ README.md
@@ -3,3 +3,44 @@ a c++11 implementation of thread pool
 
 ### easy-usage
 
+```c++
+// 1. include the thread_pool file
+#include "thread_pool.h"
+
+// 2. initialize thread pool
+int threadNum = 10;
+ThreadPool pool(threadNum);
+
+// 3. assign the task
+std::future<int> res = pool.AddTask([]{ 
+	int i = 2;
+  return i * i;
+})
+  
+// 4. get the result by std::future
+int ans = res.get();
+```
+
+
+
+### auto-seal
+
+```c++
+// 1. use lambda function
+for(int i = 0; i < 10; ++i) {
+    results.emplace_back(
+        pool.AddTask([i]{
+          return i*i;
+        })
+    );
+}
+
+// 2. use declined function
+for(int i = 0; i < 10; ++i) {
+    results.emplace_back(
+        pool.AddTask(square, i)
+    );
+}
+
+```
+
--- main.cpp
+++ main.cpp
@@ -3,20 +3,30 @@
 
 #include "thread_pool.h"
 
+int square(int i){
+    return i*i;
+}
+
 int main ()
 {
 
     ThreadPool pool(4);
     std::vector< std::future<int> > results;
 
-    for(int i = 0; i < 8; ++i) {
+//    for(int i = 0; i < 8; ++i) {
+//        results.emplace_back(
+//            pool.AddTask([i] {
+//                std::cout << "hello " << i << std::endl;
+//                std::this_thread::sleep_for(std::chrono::seconds(1));
+//                std::cout << "world " << i << std::endl;
+//                return i*i;
+//            })
+//        );
+//    }
+
+    for(int i = 0; i < 10; ++i) {
         results.emplace_back(
-            pool.AddTask([i] {
-                std::cout << "hello " << i << std::endl;
-                std::this_thread::sleep_for(std::chrono::seconds(1));
-                std::cout << "world " << i << std::endl;
-                return i*i;
-            })
+            pool.AddTask(square, i)
         );
     }
 
